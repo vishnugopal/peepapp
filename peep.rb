b56313a0625b1e17ab51d9632d4ec7d97d028a2d
Vishnu Gopal <vishnu@mobme.in>
2009-05-02 17:26:36 +0530

    Everything works, initial deploy test.

--- peep.rb
+++ peep.rb
@@ -2,14 +2,33 @@ require 'rubygems'
 require 'sinatra'
 
 require 'starling'
+require 'json'
 
-starling = Starling.new('127.0.0.1:22122')
+starling = Starling.new 'localhost:22122' #for worker data
+memcache = MemCache.new 'localhost:11211' #for master data
 
-get '/post/*' do
-  starling.set("love", params[:splat].to_s)
-  "STORED"
+post '/worker_send' do
+  queue_name = "worker_to_master_#{params[:job]}"
+  starling.set(queue_name, params.to_json)
+  params.inspect.to_s
 end
 
-get '/get' do
-  starling.get("love") if starling.sizeof("love") > 0 
+post '/worker_receive' do
+  key_name = "master_to_worker_#{params[:job]}"
+  memcache.get(key_name)
+end
+
+post '/master_send' do
+  key_name = "master_to_worker_#{params[:job]}"
+  memcache.set(key_name, params.to_json)
+  params.inspect.to_s
+end
+
+post '/master_receive' do
+  queue_name = "worker_to_master_#{params[:job]}"
+  data = []
+  while starling.sizeof(queue_name) > 0
+    data << starling.get(queue_name)
+  end
+  data.to_json
 end
